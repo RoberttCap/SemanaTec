2c6d46df25ec5e7a413856a785e4e493e7e4a196
Roberto Carlos <A01277764@tec.mx>
2024-05-07 19:49:27 -0600

    Actualizar salida de promedio

--- promedio.py
+++ promedio.py
@@ -2,5 +2,5 @@ def promedio(numeros) :
   for num in numeros :
     suma += num
   return suma/len(numeros)
-print(f"El promedio es: {promedio([1, 2, 3])}")
+print(f"El promedio de (1, 2, 3): {promedio([1, 2, 3])}")
  
